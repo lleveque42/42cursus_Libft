abda1f259ab8f19bf0bb2289436b5c66a407883b
louislvq <louis.leveque@outlook.com>
2021-11-23 11:31:50 +0100

    bzero done

---
+++ Libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 10:50:40 by lleveque          #+#    #+#             */
+/*   Updated: 2021/11/23 11:29:12 by lleveque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+// #include <>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int	ft_isascii(int c);
+int ft_isprint(int c);
+size_t strlen(const char *s);
+
+void	ft_bzero(void *s, size_t n);
+
+#endif
