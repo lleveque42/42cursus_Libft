18792cc4aecb11a5d3def2045d99aeb6d8fe0ded
louislvq <louis.leveque@outlook.com>
2021-11-23 17:21:17 +0100

    memcpy + main

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,23 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 08:48:41 by lleveque          #+#    #+#             */
-/*   Updated: 2021/11/23 14:21:30 by lleveque         ###   ########.fr       */
+/*   Created: 2021/11/23 14:22:10 by lleveque          #+#    #+#             */
+/*   Updated: 2021/11/23 17:18:47 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (i < n)
+	{
+		((char*)dest)[i] = ((char*)src)[i];
 		i++;
-	return (i);
+	}
+	((char*)dest)[i] = '\0';
+	return (dest);
 }
