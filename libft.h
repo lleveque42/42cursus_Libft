e8c21427125f9119676ba0b5348dc02a64b7db4e
louislvq <louis.leveque@outlook.com>
2021-11-23 13:20:04 +0100

    maj libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:50:40 by lleveque          #+#    #+#             */
-/*   Updated: 2021/11/23 11:51:28 by lleveque         ###   ########.fr       */
+/*   Updated: 2021/11/23 13:18:38 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int	ft_isascii(int c);
-int ft_isprint(int c);
-size_t strlen(const char *s);
-
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 
 #endif
