564bcfe897108ae181ad08a9327727447c0ec38c
louislvq <louis.leveque@outlook.com>
2021-11-24 16:39:35 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 13:52:25 by lleveque          #+#    #+#             */
-/*   Updated: 2021/11/24 16:15:18 by lleveque         ###   ########.fr       */
+/*   Created: 2021/11/24 15:50:14 by lleveque          #+#    #+#             */
+/*   Updated: 2021/11/24 16:35:52 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 
 // size_t	ft_strlen(const char *s);
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
+	size_t	len_src;
+	size_t	len_dst;
 
 	i = 0;
+	j = 0;
+	len_src = ft_strlen(src);
+	len_dst = ft_strlen(dst);
+	if (size <= len_dst)
+		return (len_src + size);
 	if (size > 0)
 	{
+		while (dst && dst[i])
+			i++;
 		while (src && src[i] && i < (size - 1))
 		{
-			dst[i] = src[i];
-			i++;
+			dst[i + j] = src[j];
+			j++;
 		}
 		dst[i] = '\0';
 	}
-	return (ft_strlen(src));
+	return (len_src + len_dst);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:50:40 by lleveque          #+#    #+#             */
-/*   Updated: 2021/11/24 14:57:29 by lleveque         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:08:57 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
